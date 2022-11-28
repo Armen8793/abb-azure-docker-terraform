2e99993b1e9ad4fc4c5a3707291849ae5d888c35
Armen8793 <tatevik.gyozalyan.93@mail.ru>
2022-11-28 23:33:08 +0400

    update variables

--- vars.tf
+++ vars.tf
@@ -20,21 +20,6 @@ variable "size" {
 }
 
 
-variable "docker_reg_url" {
-  description = "my docker image url"
-  type = string
-  default = "https://hub.docker.com/repository/docker/armen8793/abbtask"
-}
-
-variable "docker_login" {
-  description = "my docker registry username"
-  type = string
-}
-
-variable "docker_pwd" {
-  description = "my docker registry password"
-  type     = string
-}
 
 
 
